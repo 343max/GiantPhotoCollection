e37c5efa6727a0ecc338655d720c4694a4d1eae7
Max von Webel <max@343max.de>
2014-07-31 19:17:54 +0200

    added a collection view controller

---
+++ GiantPhotoCollection/PhotoCollectionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCollectionViewController.swift
+//  GiantPhotoCollection
+//
+//  Created by Max von Webel on 31/07/14.
+//  Copyright (c) 2014 Max von Webel. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewController: UICollectionViewController {
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+
+}
