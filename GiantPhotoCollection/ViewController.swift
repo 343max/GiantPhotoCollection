c39b742b156be331ae0e5b146df862a179da450f
Max von Webel <max@343max.de>
2014-07-31 18:52:09 +0200

    Initial Commit

---
+++ GiantPhotoCollection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GiantPhotoCollection
+//
+//  Created by Max von Webel on 31/07/14.
+//  Copyright (c) 2014 Max von Webel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
